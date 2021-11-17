68d8bcac73b01e5725d4b2a0dac5fd82880a3165
Brittany Ridley <britt.ridley@outlook.com>
2021-11-17 14:24:06 -0600

    UnitTesting

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,77 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1,2,3,6)]
+        [InlineData(5,5,5,15)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
-
+            var actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5,5,0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var test1 = new UnitTestMethods();
 
             //Act
-
+            var actual = test1.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1,4 ,5)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test2 = new UnitTestMethods();
             //Act
-
+            var actual = test2.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5 ,6 ,7)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test3 = new UnitTestMethods();
             //Act
-
+            var actual = UnitTestMethods.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
             //Arrange
-
+            var expected = "BNR";
+            var test4 = new UnitTestMethods();
             //Act
-
+            var actual = test4.YourMethodName();
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
+        //[Fact]
+        //public void YourMethodName2()
+        //{
+        //    var expected = "BIG EYES";
+        //    var test5 = new UnitTestMethods();
+        //    //Act
+        //    var actual = test5.YourMethodName();
+        //    //Assert
+        //    Assert.Equal(expected, actual);
+        //}
     }
 }
